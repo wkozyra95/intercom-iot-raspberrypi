0e70db75e3d12572b39e104f75c8207cea700290
wkozyra95 <wkozyra95@gmail.com>
2017-04-19 21:31:36 +0200

    not working

--- intercom.py
+++ intercom.py
@@ -10,7 +10,7 @@ def listen():
 
 def send_notification():
 
-    gcm = GCM("AAAAarr-B5Q:APA91bECRbwFs-raUrOYzFlQ-7Wb5Hk6IdDL3ZYVMzeyNIf2K6dCL9rimTXBTaGlJ1GNpXgFy5Kvfi6CxvXWvLagounBvczpBMbAj6z_gw4Vyy3DxZ4PTsbQfQiHbmnu1FIsHC4ssPDU")
-    gcm.send_topic_message(topic='INTERCOM', data={'message': 'Someone is calling!'})
+    gcm = GCM("AAAAyZ0c4Jc:APA91bHfS0tqve6u-GQ8Rh6Vfr0wU7Ufm3z7WbOhn6fdD4hERQUsbnoiDKzMZN7cGEgUmKwSYzvxXz7yfOY3ckaWW-pK9bTchmYC6gOKby3GobCJonnrLSzTb0ih3jX4eeYBdf0mwGA9")
+    gcm.plaintext_request(registration_id='f-9txMAakpY:APA91bE0ZirUhL9zEO-fNZymsQZK_Ft-GA8bWQz4ZcpGymn7hCPb4oAxHBcDIz8HDz7YZ3JUILNkOszzm_HevEvaEK23HjmMUHIoOPa9Flqzai807cyVCmvu23yxdlVIza-6hmw32v86', data={'title': 'Someone is calling!', 'body': 'wfrwfwe'})
 
 
