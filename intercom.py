437d2ee281c2134a77d835038706052be4798948
wkozyra95 <wkozyra95@gmail.com>
2017-04-20 00:18:41 +0200

    working version

--- intercom.py
+++ intercom.py
@@ -1,5 +1,15 @@
 
-from gcm import *
+from pyfcm import FCMNotification
+gcm_key = "AAAAyZ0c4Jc:APA91bHfS0tqve6u-GQ8Rh6Vfr0wU7Ufm3z7WbOhn6fdD4hERQUsbnoiDKzMZN7cGEgUmKwSYzvxXz7yfOY3ckaWW-pK9bTchmYC6gOKby3GobCJonnrLSzTb0ih3jX4eeYBdf0mwGA9"
+push_service = FCMNotification(api_key=gcm_key)
+
+# OR initialize with proxies
+
+
+registration_id = "<device registration_id>"
+message_title = "Uber update"
+message_body = "Hi john, your customized news for today is ready"
+
 
 def listen():
     while True:
@@ -9,8 +19,10 @@ def listen():
 
 
 def send_notification():
+    reg_id = 'f-9txMAakpY:APA91bE0ZirUhL9zEO-fNZymsQZK_Ft-GA8bWQz4ZcpGymn7hCPb4oAxHBcDIz8HDz7YZ3JUILNkOszzm_HevEvaEK23HjmMUHIoOPa9Flqzai807cyVCmvu23yxdlVIza-6hmw32v86'
+    data={'title': 'Someone is calling!', 'body': 'Click to show'}
 
-    gcm = GCM("AAAAyZ0c4Jc:APA91bHfS0tqve6u-GQ8Rh6Vfr0wU7Ufm3z7WbOhn6fdD4hERQUsbnoiDKzMZN7cGEgUmKwSYzvxXz7yfOY3ckaWW-pK9bTchmYC6gOKby3GobCJonnrLSzTb0ih3jX4eeYBdf0mwGA9")
-    gcm.plaintext_request(registration_id='f-9txMAakpY:APA91bE0ZirUhL9zEO-fNZymsQZK_Ft-GA8bWQz4ZcpGymn7hCPb4oAxHBcDIz8HDz7YZ3JUILNkOszzm_HevEvaEK23HjmMUHIoOPa9Flqzai807cyVCmvu23yxdlVIza-6hmw32v86', data={'title': 'Someone is calling!', 'body': 'wfrwfwe'})
+    push_service.notify_single_device(registration_id=reg_id, message_title=data['title'], message_body=data['body'])
+
 
 
